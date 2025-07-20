e7f6d8128da59fe35ce1f3608eb96fe52bc08f87
HARKAOUI Hajar <hajar15harkaoui@gmail.com>
2025-07-20 15:11:32 +0100

    update database schema

--- src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
@@ -1,12 +1,10 @@
 package ma.enset.ebankingbackend;
 
 import ma.enset.ebankingbackend.entities.*;
-import ma.enset.ebankingbackend.enums.AccountStatus;
-import ma.enset.ebankingbackend.enums.OperationType;
-import ma.enset.ebankingbackend.repositories.AccountOperationRepository;
-import ma.enset.ebankingbackend.repositories.BankAccountRepository;
-import ma.enset.ebankingbackend.repositories.CustomerRepository;
-import ma.enset.ebankingbackend.services.BankAccountService;
+import ma.enset.ebankingbackend.enums.*;
+import ma.enset.ebankingbackend.exceptions.*;
+import ma.enset.ebankingbackend.repositories.*;
+import ma.enset.ebankingbackend.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Stream;
 
