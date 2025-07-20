fa96705837e7c94a53ed2bdd5e16377e4efe9354
HARKAOUI Hajar <hajar15harkaoui@gmail.com>
2025-07-20 13:37:00 +0100

    add services

--- src/main/java/org/sid/ebankingbackend/services/BankAccountService.java
+++ src/main/java/org/sid/ebankingbackend/services/BankAccountService.java
@@ -1,36 +1,46 @@
-package org.sid.ebankingbackend.services;
+package ma.enset.ebankingbackend.services;
 
-import org.sid.ebankingbackend.dtos.*;
-import org.sid.ebankingbackend.entities.BankAccount;
-import org.sid.ebankingbackend.entities.CurrentAccount;
-import org.sid.ebankingbackend.entities.Customer;
-import org.sid.ebankingbackend.entities.SavingAccount;
-import org.sid.ebankingbackend.exceptions.BalanceNotSufficientException;
-import org.sid.ebankingbackend.exceptions.BankAccountNotFoundException;
-import org.sid.ebankingbackend.exceptions.CustomerNotFoundException;
+import ma.enset.ebankingbackend.dtos.*;
+import ma.enset.ebankingbackend.entities.*;
+import ma.enset.ebankingbackend.enums.AccountStatus;
+import ma.enset.ebankingbackend.exceptions.BalanceNotSufficientException;
+import ma.enset.ebankingbackend.exceptions.BankAccountNotFoundException;
+import ma.enset.ebankingbackend.exceptions.CustomerNotFoundException;
 
+import java.time.LocalDate;
 import java.util.List;
+
 public interface BankAccountService {
-    CustomerDTO saveCustomer(CustomerDTO customerDTO);
-    CurrentBankAccountDTO saveCurrentBankAccount(double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException;
-    SavingBankAccountDTO saveSavingBankAccount(double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException;
-    List<CustomerDTO> listCustomers();
-    BankAccountDTO getBankAccount(String accountId) throws BankAccountNotFoundException;
-    void debit(String accountId, double amount, String description) throws BankAccountNotFoundException, BalanceNotSufficientException;
-    void credit(String accountId, double amount, String description) throws BankAccountNotFoundException;
-    void transfer(String accountIdSource, String accountIdDestination, double amount) throws BankAccountNotFoundException, BalanceNotSufficientException;
+    CustomerDTO saveCustomer(CustomerDTO customer);
+    CurrentAccountDTO saveCurrentBankAccount(double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException;
+    SavingAccountDTO saveSavingBankAccount(double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException;
+    List<CustomerDTO> getCustomers();
+    BankAccountDTO getBankAccountById(String id) throws BankAccountNotFoundException;
+    void debit(String id, double amount, String description) throws BankAccountNotFoundException, BalanceNotSufficientException;
+    void credit(String id, double amount, String description) throws BankAccountNotFoundException;
+    void transfer(String fromId, String toId, double amount) throws BankAccountNotFoundException, BalanceNotSufficientException;
 
-    List<BankAccountDTO> bankAccountList();
+    List<BankAccountDTO> getAllBankAccounts();
 
-    CustomerDTO getCustomer(Long customerId) throws CustomerNotFoundException;
+    CustomerDTO getCustomerById(long id) throws CustomerNotFoundException;
 
     CustomerDTO updateCustomer(CustomerDTO customerDTO);
 
-    void deleteCustomer(Long customerId);
+    void deleteCustomer(long id) throws CustomerNotFoundException;
 
-    List<AccountOperationDTO> accountHistory(String accountId);
+    List<AccountOperationDTO> getHistory(String id);
 
     AccountHistoryDTO getAccountHistory(String accountId, int page, int size) throws BankAccountNotFoundException;
 
-    List<CustomerDTO> searchCustomers(String keyword);
+    List<CustomerDTO> searchCustomer(String s);
+
+    List<BankAccountDTO> getAccountsByCustomerId(Long customerID) throws CustomerNotFoundException;
+
+    void updateAccountStatus(String id, AccountStatus status) throws BankAccountNotFoundException;
+
+    void cancelOperation(Long id) throws BankAccountNotFoundException;
+
+    AccountHistoryDTO searchOperations(String accountId, LocalDate startDate, LocalDate endDate, Double minAmount, Double maxAmount, int page, int size) throws BankAccountNotFoundException;
+
+    BankAccountDTO getLatestAccount();
 }
